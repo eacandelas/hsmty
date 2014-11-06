e61c6614d8491546a8650cc7c0ba1daa1ac061db
Eden <eden@sprite.(none)>
2014-11-06 11:56:17 -0600

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ hsmty
 =====
 
 Diferent proyects for the HackerspaceMty
+
+Each folder contains code and info related to the proyects where we help other members or externals.
+
---
+++ tweetAnalysis/tweetAuth.py
@@ -0,0 +1,17 @@
+#! -*- encoding: utf-8 -*-
+##################################################
+# script to test twitter apy authorization using
+# oauthlib for oauth process.
+#
+#
+#
+##################################################
+
+
+from __future__ import unicode_literals
+import requests
+from requests_oauthlib import OAuth1
+from urlparse import parse_qs
+
+
+
